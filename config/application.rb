747ac71f8d07b9676aa921ea43499db7ab22d795
Jan Krupa <jan.krupa@vsb.cz>
2020-01-13 09:04:48 +0100

    Gem rack-cors added to enable Cross Origin Resource Sharing (CORS)
    We restrict the origins to our front-end app at http://localhost:3000 and allow access to the standard REST API endpoint methods for all resources.

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,15 @@ module IdeaboardApi
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    # We restrict the origins to our front-end app at http://localhost:3000
+    # and allow access to the standard REST API endpoint methods for all resources
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
